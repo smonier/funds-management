58c20b8eb180a66dfa82e96e219a3ae33df561b3
Stephane Monier <smonier@jahia.com>
2024-02-26 08:15:37 +0100

    table view optimisation

--- src/main/resources/fundmgmtnt_fundsList/html/fundsList.jsp
+++ src/main/resources/fundmgmtnt_fundsList/html/fundsList.jsp
@@ -9,25 +9,52 @@
 <%@ taglib prefix="query" uri="http://www.jahia.org/tags/queryLib" %>
 <%@ taglib prefix="utility" uri="http://www.jahia.org/tags/utilityLib" %>
 <%@ taglib prefix="s" uri="http://www.jahia.org/tags/search" %>
-<%--@elvariable id="currentNode" type="org.jahia.services.content.JCRNodeWrapper"--%>
-<%--@elvariable id="out" type="java.io.PrintWriter"--%>
-<%--@elvariable id="script" type="org.jahia.services.render.scripting.Script"--%>
-<%--@elvariable id="scriptInfo" type="java.lang.String"--%>
-<%--@elvariable id="workspace" type="java.lang.String"--%>
-<%--@elvariable id="renderContext" type="org.jahia.services.render.RenderContext"--%>
-<%--@elvariable id="currentResource" type="org.jahia.services.render.Resource"--%>
-<%--@elvariable id="url" type="org.jahia.services.render.URLGenerator"--%>
+<%--@elvariable id="currentNode"
+type="org.jahia.services.content.JCRNodeWrapper" --%>
+<%--@elvariable id="out" type="java.io.PrintWriter" --%>
+<%--@elvariable id="script"
+type="org.jahia.services.render.scripting.Script" --%>
+<%--@elvariable id="scriptInfo" type="java.lang.String" --%>
+<%--@elvariable id="workspace" type="java.lang.String" --%>
+<%--@elvariable id="renderContext"
+type="org.jahia.services.render.RenderContext" --%>
+<%--@elvariable id="currentResource"
+type="org.jahia.services.render.Resource" --%>
+<%--@elvariable id="url"
+type="org.jahia.services.render.URLGenerator"
+--%>
 
-<c:url var="fundsUrl" value="https://raw.githubusercontent.com/smonier/funds-management/main/src/main/resources/files/funds/funds.json"/>
+<c:set var="title"
+    value="${currentNode.properties['jcr:title'].string}" />
+<c:set var="csvFile"
+    value="${currentNode.properties['csvFile'].node}" />
+<c:set var="jsonUrl"
+    value="${currentNode.properties['jsonUrl'].string}" />
 
+    <template:addResources
+    resources="tabulator.min.css"
+    type="css" />
+<template:addResources
+    resources="tabulator.min.js"
+    type="javascript" />
+<template:addResources
+    resources="fundsFromJson.js"
+    type="javascript" />
+<template:addResources
+    resources="tabulator_bootstrap5.css"
+    type="css" />
 
-<template:addResources resources="tabulator.min.js" type="javascript"/>
-<template:addResources resources="fundsFromJson.js" type="javascript"/>
-<template:addResources resources="tabulator_bootstrap5.css" type="css"/>
-<c:set var="title" value="${currentNode.properties['jcr:title'].string}"/>
 <h2>${title}</h2>
-<div id="myAjax-${currentNode.identifier}" class="fundsList" data-url="${fundsUrl}"></div>
-
+<c:if test="${not empty jsonUrl}">
+    <div id="myAjax-${currentNode.identifier}"
+        class="fundsList"
+        data-url="${jsonUrl}"></div>
+</c:if>
 <p>&nbsp;</p>
-<c:url var="csvFunds" value="${url.server}/sites/fund/files/funds/funds.csv"/>
-<div id="myCsv-${currentNode.identifier}" class="csvTable" data-url="${csvFunds}"></div>
+<c:if test="${not empty csvFile}">
+
+    <div id="myCsv-${currentNode.identifier}"
+        class="csvTable"
+        data-url="${csvFile.getUrl()}">
+    </div>
+</c:if>
--- src/main/resources/javascript/fundsFromJson.js
+++ src/main/resources/javascript/fundsFromJson.js
@@ -1,3 +1,102 @@
+//custom max min header filter
+var minMaxFilterEditor = function(cell, onRendered, success, cancel, editorParams){
+
+    var end;
+
+    var container = document.createElement("span");
+
+    //create and style inputs
+    var start = document.createElement("input");
+    start.setAttribute("type", "number");
+    start.setAttribute("placeholder", "Min");
+    start.setAttribute("min", 0);
+    start.setAttribute("max", 100);
+    start.style.padding = "4px";
+    start.style.width = "50%";
+    start.style.boxSizing = "border-box";
+
+    start.value = cell.getValue();
+
+    function buildValues(){
+        success({
+            start:start.value,
+            end:end.value,
+        });
+    }
+
+    function keypress(e){
+        if(e.keyCode == 13){
+            buildValues();
+        }
+
+        if(e.keyCode == 27){
+            cancel();
+        }
+    }
+
+    end = start.cloneNode();
+    end.setAttribute("placeholder", "Max");
+
+    start.addEventListener("change", buildValues);
+    start.addEventListener("blur", buildValues);
+    start.addEventListener("keydown", keypress);
+
+    end.addEventListener("change", buildValues);
+    end.addEventListener("blur", buildValues);
+    end.addEventListener("keydown", keypress);
+
+
+    container.appendChild(start);
+    container.appendChild(end);
+
+    return container;
+ }
+
+//custom max min filter function
+function minMaxFilterFunction(headerValue, rowValue, rowData, filterParams){
+    //headerValue - the value of the header filter element
+    //rowValue - the value of the column in this row
+    //rowData - the data for the row being filtered
+    //filterParams - params object passed to the headerFilterFuncParams property
+
+        if(rowValue){
+            if(headerValue.start != ""){
+                if(headerValue.end != ""){
+                    return rowValue >= headerValue.start && rowValue <= headerValue.end;
+                }else{
+                    return rowValue >= headerValue.start;
+                }
+            }else{
+                if(headerValue.end != ""){
+                    return rowValue <= headerValue.end;
+                }
+            }
+        }
+
+    return true; //must return a boolean, true if it passes the filter.
+}
+
+
+function csvToJson(csvString) {
+    const lines = csvString.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split('\n');
+
+    // Extract headers, removing quotes
+    const headers = lines[0].split(',').map(header => header.replace(/"/g, ''));
+
+    // Map the rows to objects
+    const result = lines.slice(1).map(line => {
+        const data = line.split(',').map(field => field.replace(/"/g, ''));
+        return headers.reduce((obj, nextKey, index) => {
+            obj[nextKey] = data[index];
+            return obj;
+        }, {});
+    });
+
+    // Return the JSON object
+    return result;
+}
+
+
 
 $(document).ready(function () {
     $(".fundsList").each(function () {
@@ -41,22 +140,27 @@ $(document).ready(function () {
             type: 'GET',
             dataType: 'text',
             success: function (csvData) {
-                new Tabulator(selector, {
-                    data: csvData, // provide the fetched CSV data
+                var jsonFromCsv = csvToJson(csvData);
+                var table = new Tabulator(currentID, {
+                    data: jsonFromCsv, // provide the fetched CSV data
                     layout: "fitColumns", // Fit columns to width of table
                     pagination: "local", // Enable local pagination
                     paginationSize: 10, // Set number of rows per page
                     placeholder: "No Data Available", // Display message when no data is available
+                    responsiveLayout:"collapse",
+                    groupBy:["FUND NAME"],
+                    paginationCounter:"rows",
                     columns: [
-                        // {title: "Asset Class", field: "asset_class"}, //column has a fixed width of 100px;
-                        { title: "Fund Name", field: "FUND NAME" }, //column has a fixed width of 100px;
+                        { formatter:"responsiveCollapse", width:30, minWidth:30, hozAlign:"center", resizable:false, headerSort:false},
+                        { title: "Fund Name", field: "FUND NAME", responsive:0, headerFilter:"input" }, //column has a fixed width of 100px;
+                        { title: "Asset Class", field: "ASSET CLASS",  editor:"list", headerFilter:true, headerFilterParams:{values:{"Equity":"Equity", "Fixed Income":"Fixed Income", "Alternative":"Alternative", "Multi-Asset":"Multi-Asset", "":""}, clearable:true}}, //column has a fixed width of 100px;
                         { title: "Share Class", field: "SHARE CLASS" }, //column has a fixed width of 100px;
                         { title: "ISIN", field: "ISIN" }, //column has a fixed width of 100px;
-                        { title: "Inception Date", field: "INCEPTION DATE" }, //column has a fixed width of 100px;
-                        { title: "AUM", field: "AUM" }, //column has a fixed width of 100px;
-                        { title: "As of Date", field: "AS OF DATE" }, //column has a fixed width of 100px;
+                        { title: "Inception Date", field: "INCEPTION DATE", responsive:3 }, //column has a fixed width of 100px;
+                        { title: "AUM", field: "AUM", responsive:3 }, //column has a fixed width of 100px;
+                        { title: "As of Date", field: "AS OF DATE", responsive:3 }, //column has a fixed width of 100px;
                         { title: "MorningStar", field: "MORNINGSTAR", formatter: "star", hozAlign: "center", width: 100, editor: true },
-                        { title: "SFDR Classification", field: "SFDR ARTICLE CLASSIFICATION" }, //column has a fixed width of 100px;
+                        { title: "SFDR Classification", field: "SFDR ARTICLE CLASSIFICATION", responsive:2 }, //column has a fixed width of 100px;
 
                     ],
                 });
